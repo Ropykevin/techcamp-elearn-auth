f369fd396451f96c5abcc5ae690abe2b70d87c91
Ropy Kevin <ropykevin@gmail.com>
2024-08-05 00:50:48 -0900

    update

--- app/main.py
+++ app/main.py
@@ -3,20 +3,17 @@ from authlib.integrations.flask_client import OAuth
 from authlib.common.security import generate_token
 import os
 from dotenv import load_dotenv
-import requests
-
-
-API_ENDPOINT = "http://167.71.54.75:8082/trainees"
 
 app = Flask(__name__)
 app.secret_key = 'YOUR_SECRET_KEY'
-oauth=OAuth(app)
+oauth = OAuth(app)
 
 load_dotenv()
 # OAuth 2 client setup
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
-GOOGLE_DISCOVERY_URL = ("https://accounts.google.com/.well-known/openid-configuration")
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration")
 print(GOOGLE_CLIENT_ID)
 oauth.register(
     name='google',
@@ -33,45 +30,53 @@ oauth.register(
 def index():
     return 'You are not logged in.'
 
+
 @app.route('/login')
 def login():
-    # put a url with an https here
-    redirect_uri = "https://learn.techcamp.co.ke/login/callback"
+    redirect_uri = ""  # put a url with an https here
     print(redirect_uri)
-    session["nonce"]=generate_token()
-    return oauth.google.authorize_redirect(redirect_uri,nonce=session["nonce"])
+    session["nonce"] = generate_token()
+    return oauth.google.authorize_redirect(redirect_uri, nonce=session["nonce"])
 
 
 @app.route("/login/callback")
 def callback():
     token = oauth.google.authorize_access_token()
     google_user = oauth.google.parse_id_token(token, nonce=session["nonce"])
-    email = google_user['email']
-    full_name = google_user['name']
-    # all trainees
-    response = requests.get(API_ENDPOINT)
-    if response.status_code != 200:
-        return "Error fetching trainees from external API", 500
-    trainees = response.json()
-    # Check if email exists in the list of trainees
-    user = next(
-        (trainee for trainee in trainees if trainee['email'] == email), None)
-    if user:
-        return user
-    else:
-        # If user does not exist
-        payload = {
-            "email": email,
-            "firebaseId": "web",
-            "fullName": full_name,
-            "id": 0,
-            "latestDeviceId": "web"
-        }
-        response = requests.post(API_ENDPOINT, json=payload)
-        if response.status_code != 201:
-            return "Error storing user in external API", 500
-        user = response.json()
-        return user
+    return google_user['email']
+
+
+# @app.route("/login/callback")
+# def callback():
+#     token = oauth.google.authorize_access_token()
+#     google_user = oauth.google.parse_id_token(token, nonce=session["nonce"])
+#     email = google_user['email']
+#     full_name = google_user['name']
+#     # all trainees
+#     response = requests.get(API_ENDPOINT)
+#     if response.status_code != 200:
+#         return "Error fetching trainees from external API", 500
+#     trainees = response.json()
+#     # Check if email exists in the list of trainees
+#     user = next(
+#         (trainee for trainee in trainees if trainee['email'] == email), None)
+#     if user:
+#         return user
+#     else:
+#         # If user does not exist
+#         payload = {
+#             "email": email,
+#             "firebaseId": "web",
+#             "fullName": full_name,
+#             "id": 0,
+#             "latestDeviceId": "web"
+#         }
+#         response = requests.post(API_ENDPOINT, json=payload)
+#         if response.status_code != 201:
+#             return "Error storing user in external API", 500
+#         user = response.json()
+#         return user
+
 
 if __name__ == "__main__":
     app.run(debug=True)
